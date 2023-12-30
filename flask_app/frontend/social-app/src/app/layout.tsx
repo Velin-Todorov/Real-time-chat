21d9ae28882c101e5c17e8a29fa202295d65671c
Velin-Todorov <velintodorov@yahoo.com>
2023-12-30 09:51:19 +0100

    small change

--- flask_app/frontend/social-app/src/app/layout.tsx
+++ flask_app/frontend/social-app/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-lime-300`}>{children}</body>
+      <body className={`${inter.className} min-h-screen`}>{children}</body>
     </html>
   )
 }
